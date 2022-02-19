233d85aab33734923194e72819a9c5fae6213343
Andrea-gt <a.ximeram@gmail.com>
2022-02-19 09:08:53 -0600

    Evaluating Expressions with Stacks

--- controlador.java
+++ controlador.java
@@ -4,38 +4,50 @@
 
 /**
  * @author Andrea
- *
+ * @version 19/02/2022
+ * 
  */
-public class controlador implements IStack {
+public class controlador {
 
-	@Override
-	public void push(Object value) {
-		// TODO Auto-generated method stub
-		
+	Stack <Integer> stack =new Stack<>();
+
+	public <T> int EvaluarLinea (String expresion) {
+
+		for(int i=0;i<expresion.length();i++)
+		{
+			char datoPrimitivo = expresion.charAt(i);
+
+			//Si el dato es un numero
+			if(Character.isDigit(datoPrimitivo)) {
+				stack.push(datoPrimitivo - '0');
+			}
+
+			//Si el dato es un operando
+			else { 
+
+				int value_1 = stack.pull();
+				int value_2 = stack.pull();
+
+				switch(datoPrimitivo) {
+
+				case '+':
+					stack.push(value_1+value_2);
+					break;
+
+				case '-':
+					stack.push(value_1 - value_2);
+					break;
+
+				case '*':
+					stack.push(value_1 * value_2);
+					break;
+
+				case '/':
+					stack.push(value_1 / value_2);
+					break;
+				}
+			}
+		}
+		return stack.pull();
 	}
-
-	@Override
-	public Object pull() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object peek() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
--- vista.java
+++ vista.java
@@ -1,37 +1,46 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.Scanner;
 
 public class vista {
 
 	public static void main(String[] args) {
-		int linea = 0;
+		controlador ctr = new controlador();
 		FileInputStream fstream;
+		
+		System.out.println("Calculadora para expresiones Postfix");
+		Scanner sn = new Scanner(System.in); 
+		System.out.println("\nPor favor ingrese el enlace del archivo a evaluar");
+		String path = sn.nextLine();
+		
 		try {
-			fstream = new FileInputStream("test.txt");
+			fstream = new FileInputStream(path);
 			BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-
+			
 			String strLine;
 
 			//Leer Archivo Linea por Linea
 			while ((strLine = br.readLine()) != null)   {
 				
-				// Imprimir Resultados
-				System.out.println (linea +". "+ strLine);
-				linea++;
+				// Evaluar Linea
+				System.out.println ("Resultado de " + strLine + ": " + ctr.EvaluarLinea(strLine));
 			}
 
 			//Cerrar el input
 			fstream.close();
 		}
 		catch (FileNotFoundException e) {
+			System.out.println("Dirección Inválida");
 			e.printStackTrace();
 		}
 		catch (IOException e) {
 			e.printStackTrace();
+		}
+		catch(ArithmeticException e) {
+			System.out.println("La expresion es indefinida");
 		}
 	}
 }
